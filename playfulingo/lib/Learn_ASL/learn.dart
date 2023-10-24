a1b254a35ef69a1d2d3dd3229ff360d42d5dc877
JinLin6398 <jinlin11220@gmail.com>
2023-10-23 21:28:38 -0400

    add assets and add fill in the blank game

--- playfulingo/lib/Learn_ASL/learn.dart
+++ playfulingo/lib/Learn_ASL/learn.dart
@@ -1,27 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:playfulingo/Learn_ASL/flashcard.dart';
+
+
+class LearnItem extends StatelessWidget {
+  final String title;
+  final String image;
+  final Widget nextScreen;
+
+  const LearnItem(
+      {super.key,
+      required this.title,
+      required this.image,
+      required this.nextScreen});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          // Navigate to the desired screen when the button is pressed
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => nextScreen,
+            ),
+          );
+        },
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.zero,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(image),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Colors.white.withOpacity(0.5),
+                  BlendMode.lighten,
+                ),
+              ),
+              border: Border.all(color: Colors.black, width: 10.0),
+              borderRadius: BorderRadius.circular(20.0)),
+          child: Center(
+            child: Text(
+              title,
+              style: const TextStyle(
+                color: Colors.red,
+                fontSize: 35.0,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold,
+                shadows: [
+                  Shadow(
+                    blurRadius: 4.0,
+                    color: Colors.black,
+                    offset: Offset(2.0, 2.0),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+
+
 
 class LearnScreen extends StatelessWidget {
+  const LearnScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Learn Screen'),
+        title: const Text('Playfulingo'),
+        backgroundColor: Colors.black,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Welcome to Learn Screen!',
-              style: TextStyle(fontSize: 20.0),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate back to the previous screen
-                Navigator.of(context).pop();
-              },
-              child: Text('Go Back'),
+      body: Padding(
+        padding:  const EdgeInsets.all(8.0),
+        child: GridView.count(
+          crossAxisCount: 2, // Two columns
+          mainAxisSpacing: 20.0,
+          crossAxisSpacing: 20.0,
+          children:  <Widget>[ 
+            LearnItem(
+                title: 'Flashcard',
+                image: 'assets/flashcard.png',
+                nextScreen: FlashcardScreen(flashcards: flashcards)
             ),
           ],
         ),
@@ -29,3 +96,4 @@ class LearnScreen extends StatelessWidget {
     );
   }
 }
+
