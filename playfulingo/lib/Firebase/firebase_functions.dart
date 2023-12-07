3033ba57804c285898f6d70ab1767eb6685ff935
shahks-hub <kasaf.shah28@myhunter.cuny.edu>
2023-12-06 19:26:10 -0500

    adding db

--- playfulingo/lib/Firebase/firebase_functions.dart
+++ playfulingo/lib/Firebase/firebase_functions.dart
@@ -7,18 +7,18 @@ Future<List<String>> fetchCompletedLessons(String userEmail) async {
         .where('email', isEqualTo: userEmail)
         .limit(1)
         .get();
-
+   
     List<String> completedLessons = [];
-
+    //  print(userSnapshot.docs);
     if (userSnapshot.docs.isNotEmpty) {
       final userData = userSnapshot.docs.first.data();
-
+      //  print("user data: $userData ");
       List<dynamic> completedLessonsRefs = userData['completed_lessons'] ?? [];
 
       // Ensure that elements are casted to strings
       completedLessons.addAll(completedLessonsRefs.map((ref) => ref.id.toString()));
     }
-
+    // print("completed: $completedLessons");
     return completedLessons;
   } catch (e) {
     print('Error fetching completed lessons: $e');
--- playfulingo/lib/Games/game.dart
+++ playfulingo/lib/Games/game.dart
@@ -21,14 +21,14 @@ class GameScreen extends StatelessWidget {
   Widget build(BuildContext context) {
      final currentUser = FirebaseAuth.instance.currentUser;
      return FutureBuilder<List<String>>(
-      future: fetchCompletedLessons(currentUser?.uid ?? ''),
+      future: fetchCompletedLessons(currentUser?.email ?? ''),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return CircularProgressIndicator(); // Loading indicator while fetching data
         } else if (snapshot.hasError) {
           return Text('Error: ${snapshot.error}');
         } else {
-          final completedLessons = snapshot.data ?? [];
+          final completedLessons = Set.from(snapshot.data ?? []);
 
     return Scaffold(
       appBar: AppBar(
@@ -60,33 +60,38 @@ class GameScreen extends StatelessWidget {
             children: <Widget>[
                const GameItem(
               title: 'Multiple Choice',
-              image: 'assets/multiple-choice.png',
+              unlockedImage: 'assets/multiple-choice.png',
+              lockedImage:  'assets/lock.png'  ,
               nextScreen: multipleChoice(),
                isUnlocked: true, 
             ),
             GameItem(
                 title: 'Fill In The Blanks',
-                image: 'assets/fill_in_the blank.png',
+               unlockedImage:  'assets/fill_in_the blank.png'  ,
+               lockedImage:  'assets/lock.png'  ,
                 nextScreen: FillInTheBlank(),
                  isUnlocked: true, 
             ),
             GameItem(
                 title: 'yes_or_no',
-                image: 'assets/yes_or_no.png',
+                unlockedImage: 'assets/yes_or_no.png',
+                lockedImage:    'assets/lock.png' ,
                 nextScreen: YesNoGame(),
                  isUnlocked: true, 
             ),
             GameItem(
               title: 'Alphabet Drag Drop Match',
-              image: 'assets/flashcard.png',
+              unlockedImage: 'assets/flashcard.png' ,
+              lockedImage:   'assets/lock.png' ,
               nextScreen: ASLMatchingGame(),
-               isUnlocked: completedLessons.contains('lesson_id_1'), 
+               isUnlocked: completedLessons.contains('x3BsaHPxcvmBoQLroe0I'), 
             ),
               GameItem(
                 title: 'Snap and Prac',
-                image: 'assets/learn_bg.png',
+                unlockedImage: 'assets/learn_bg.png',
+                lockedImage:   'assets/lock.png' ,
                 nextScreen: CameraScreen(),
-                 isUnlocked: false, 
+                 isUnlocked: completedLessons.contains('x3BsaHPxcvmBoQLroe0I'), 
             ),
          
             ElevatedButton(
@@ -123,37 +128,33 @@ class GameScreen extends StatelessWidget {
     );
   }
 }
-
-
-
-
-
 class GameItem extends StatelessWidget {
   final String title;
-  final String image;
+  final String? unlockedImage;
+  final String? lockedImage;
   final Widget nextScreen;
-    final bool isUnlocked;
+  final bool isUnlocked;
 
-  const GameItem(
-      {super.key,
-      required this.title,
-      required this.image,
-      required this.nextScreen,
-      required this.isUnlocked,
-      });
+  const GameItem({
+    required this.title,
+    required this.unlockedImage,
+    required this.lockedImage,
+    required this.nextScreen,
+    required this.isUnlocked,
+  });
 
   @override
   Widget build(BuildContext context) {
-    
     return ElevatedButton(
-      onPressed:isUnlocked?() {
-        // Navigate to the desired screen when the button is pressed
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => nextScreen,
-          ),
-        );
-      }:null,
+      onPressed: isUnlocked
+          ? () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => nextScreen,
+                ),
+              );
+            }
+          : null,
       style: ElevatedButton.styleFrom(
         padding: EdgeInsets.zero,
         shape: RoundedRectangleBorder(
@@ -164,7 +165,7 @@ class GameItem extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage(image),
+            image: AssetImage(isUnlocked ? unlockedImage! : lockedImage!),
             fit: BoxFit.cover,
             colorFilter: ColorFilter.mode(
               Colors.white.withOpacity(0.5),
@@ -177,124 +178,33 @@ class GameItem extends StatelessWidget {
             colors: [Colors.blue, Colors.white, Colors.red],
           ),
         ),
-        child: Center(
-          child: Text(
-            title,
-            style: const TextStyle(
-              color: Colors.red,
-              fontSize: 30.0,
-              fontStyle: FontStyle.normal,
-              fontWeight: FontWeight.bold,
-              shadows: [
-                Shadow(
-                  blurRadius: 4.0,
-                  color: Colors.black,
-                  offset: Offset(2.0, 2.0),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-
-
-class GameScreen extends StatelessWidget {
-  const GameScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-         iconTheme: IconThemeData(
-      color: Colors.blue, // Change the color of the back arrow here
-    ),
-        title: const Text(
-          'Practice make perfect', 
-          style: TextStyle(
-            color: Colors.orange,
-            fontSize: 30.0,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.bold,
-            ),),
-        backgroundColor: Colors.black,
-      ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          gradient: linearGradient(45, ['blue', 'green', 'red']),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: GridView.count(
-            crossAxisCount: 2, // Two columns
-            mainAxisSpacing: 20.0,
-            crossAxisSpacing: 20.0,
-            children: <Widget>[
-               const GameItem(
-              title: 'Multiple Choice',
-              image: 'assets/multiple-choice.png',
-              nextScreen: multipleChoice(),
-            ),
-            GameItem(
-                title: 'Fill In The Blanks',
-                image: 'assets/fill_in_the blank.png',
-                nextScreen: FillInTheBlank()
-            ),
-            GameItem(
-                title: 'yes_or_no',
-                image: 'assets/yes_or_no.png',
-                nextScreen: YesNoGame()
-            ),
-            GameItem(
-              title: 'Alphabet Drag Drop Match',
-              image: 'assets/flashcard.png',
-              nextScreen: ASLMatchingGame()
-            ),
-              GameItem(
-                title: 'Snap and Prac',
-                image: 'assets/learn_bg.png',
-                nextScreen: CameraScreen()
-            ),
-         
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => Dash(),
+        child: isUnlocked
+            ? Center(
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    color: Colors.red,
+                    fontSize: 30.0,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.bold,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 4.0,
+                        color: Colors.black,
+                        offset: Offset(2.0, 2.0),
+                      ),
+                    ],
                   ),
-                );
-              },
-              child: Text(
-                'Go back to homepage', 
-                style: TextStyle(
-                fontSize: 20,
-                
+                ),
+              )
+            : Center(
+                child: Icon(
+                  Icons.lock,
+                  size: 40,
+                  color: Colors.black87,
+                ),
               ),
-                  
-              ),
-              style: ElevatedButton.styleFrom(
-    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 12), // Button padding
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(80), // Button border shape
-    ),
-    primary: Colors.orange[300], // Background color
-    onPrimary: Colors.blue, // Text color
-  ),
-),
-            
-            ],
-          ),
-        ),
       ),
     );
   }
 }
-
-
-
-
